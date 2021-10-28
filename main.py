be137b1ffe7fb4ea4b5d5d5ad3f61cb858bb87df
Stefan <92930744+stefangojkovic@users.noreply.github.com>
2021-10-28 09:51:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -34,12 +35,11 @@ while game_is_on:
         snake.extend()
 
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            # snake.reset()
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("score.txt", mode='r') as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -16,12 +18,19 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", font=FONT, align=ALIGNMENT)
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", font=FONT, align=ALIGNMENT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game over", font=FONT, align=ALIGNMENT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("score.txt", mode='w') as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game over", font=FONT, align=ALIGNMENT)
